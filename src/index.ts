ef5fa04e47e6f852e5e2f1c56b5f55e28397487e
Conner Ruhl <connerruhl@me.com>
2018-07-08 21:38:22 -0500

    Dynamic lights

--- src/index.ts
+++ src/index.ts
@@ -3,85 +3,6 @@ import "cross-fetch/polyfill";
 import ApolloClient from "apollo-boost";
 import gql from "graphql-tag";
 
-const client = new ApolloClient({
-  uri: "http://localhost:8080"
-});
-
-const lights = [
-  {
-    id: "1",
-    name: "Treadmill"
-  },
-  {
-    id: "2",
-    name: "Window"
-  },
-  {
-    id: "3",
-    name: "Office Lamp"
-  },
-  {
-    id: "4",
-    name: "Bathroom"
-  },
-  {
-    id: "5",
-    name: "Window"
-  },
-  {
-    id: "6",
-    name: "Living Room Lamp"
-  },
-  {
-    id: "7",
-    name: "Loveseat"
-  },
-  {
-    id: "9",
-    name: "Recliner"
-  },
-  {
-    id: "11",
-    name: "Fireplace Right"
-  },
-  {
-    id: "12",
-    name: "Counter Wall"
-  },
-  {
-    id: "13",
-    name: "Fireplace Left"
-  },
-  {
-    id: "14",
-    name: "Counter Trash"
-  },
-  {
-    id: "15",
-    name: "Strip"
-  },
-  {
-    id: "16",
-    name: "Desk"
-  },
-  {
-    id: "17",
-    name: "Wall"
-  },
-  {
-    id: "18",
-    name: "North"
-  },
-  {
-    id: "19",
-    name: "East"
-  },
-  {
-    id: "20",
-    name: "West"
-  }
-];
-
 const order = [
   "West",
   "Living Room Lamp",
@@ -89,9 +10,59 @@ const order = [
   "Fireplace Left",
   "Recliner",
   "Loveseat",
+  "Entry",
   "Counter Wall",
   "Counter Trash"
 ];
+
+const duration = 4000;
+
+const client = new ApolloClient({
+  uri: "http://localhost:8080"
+});
+
+(async () => {
+  const {
+    data: { lights }
+  } = await client.query<{
+    lights: Array<{ id: string; name: string }>;
+  }>({
+    query: gql`
+      {
+        lights {
+          id
+          name
+        }
+      }
+    `
+  });
+
+  setInterval(() => {
+    order.forEach((lightName, index) => {
+      const light = lights.find(({ name }) => lightName === name);
+
+      if (!light) {
+        return null;
+      }
+
+      setTimeout(async () => {
+        await setLight(light.id, {
+          color: "#ff0000",
+          transition: duration
+        });
+
+        setTimeout(
+          () =>
+            setLight(light.id, {
+              color: "#5400ff",
+              transition: duration * 2
+            }),
+          duration
+        );
+      }, index * duration);
+    });
+  }, order.length * duration);
+})();
 
 const setLight = async (id: string, state: object) =>
   client.mutate({
@@ -104,31 +75,3 @@ const setLight = async (id: string, state: object) =>
       }
     `
   });
-
-const duration = 4000;
-
-setInterval(async () => {
-  order.forEach(async (lightName, index) => {
-    const light = lights.find(({ name }) => lightName === name);
-
-    if (!light) {
-      return null;
-    }
-
-    setTimeout(async () => {
-      await setLight(light.id, {
-        color: "#ff0000",
-        transition: duration
-      });
-
-      setTimeout(
-        async () =>
-          setLight(light.id, {
-            color: "#5400ff",
-            transition: duration * 2
-          }),
-        duration
-      );
-    }, index * duration);
-  });
-}, order.length * duration);
